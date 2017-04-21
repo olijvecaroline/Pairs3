b9549a78fa21f1e224fc75eb216614df2eb6a01f
Caroline Olijve <olijvecaroline@gmail.com>
2017-04-21 07:06:23 +0200

    get 'remove users/:id'=> 'users#show' before start

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'users/:id'=> 'users#show'
   devise_for :users
   resources :days
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
